9b6b901d8d440a3f9deb23e25f52de7675c752ac
Chin <mcchin@gmail.com>
2015-02-12 01:00:44 +0800

    Refactor utility functions

--- sdomquery.js
+++ sdomquery.js
@@ -1,10 +1,9 @@
-! function(e) {
+! function(sDomQuery) {
     var obj;
-    "undefined" !== typeof window ? obj = window : "undefined" !== typeof global ? obj = global : "undefined" !== typeof self && (obj = self), obj.sDomQuery = obj.$ = e();
+    "undefined" !== typeof window ? obj = window : "undefined" !== typeof global ? obj = global : "undefined" !== typeof self && (obj = self), obj.sDomQuery = obj.$ = sDomQuery();
 } (function() {
-    return (function loadModule(moduleList, initFunction, api) {		
-        $ = moduleList[initFunction].call(api, moduleList, api);
-        return $;
+    return (function loadModule(moduleList, initFunction) {
+        return moduleList[initFunction].call(this, moduleList);
     })({
         modules: {
             css: function() {
@@ -13,8 +12,9 @@
                         // testing
                         console.log('act1');
                         console.log(this);
-                        this['each'](this, function(ele) {
+                        this['each'](this, function(ele, idx) {
                             console.log(ele.style);
+                            console.log(idx);
                         });						
                         return this;
                     },
@@ -24,54 +24,60 @@
                         return this;
                     }
                 };
-            },
-            utils: function() {
-                return {
-                    each: function each() {
-                        var elements = null,
-                            callback = function() {};
-
-                        if ( arguments.length > 0 ) {
-                            if ( "function" === typeof arguments[0] ) {
-                                callback = arguments[0];
-                                elements = this;
-                            } else if ( arguments.length > 1 && "function" === typeof arguments[1] ) {
-                                elements = arguments[0];
-                                callback = arguments[1];
-                            }
-
-                            var length = elements.length;
-                            if ("undefined" !== length) {
-                                for (var i = 0; i < length; i++) {
-                                    callback.call(null, elements[i], i, elements);
-                                }
-                            }
-                        }
-                        return elements;
-                    }
-                }
             }
         },
-        selector: function(api, functionList) {
+        utils: function() {
+            return {
+                each: function each() {
+                    var elements = null,
+                        callback = function() {};
 
+                    if ( arguments.length > 0 ) {
+                        if ( "function" === typeof arguments[0] ) {
+                            callback = arguments[0];
+                            elements = this;
+                        } else if ( arguments.length > 1 && "function" === typeof arguments[1] ) {
+                            elements = arguments[0];
+                            callback = arguments[1];
+                        }
+
+                        var length = elements.length;
+                        if ("undefined" !== length) {
+                            for (var i = 0; i < length; i++) {
+                                callback.call(null, elements[i], i, elements);
+                            }
+                        }
+                    }
+                    return elements;
+                },
+                extend: function (mergeTarget) {
+                  var otherObjects = [];
+
+                  for (var _key = 1; _key < arguments.length; _key++) {
+                    otherObjects[_key - 1] = arguments[_key];
+                  }
+
+                  for (var _key = 0; _key < otherObjects.length; _key++) {
+                    for (var objProp in otherObjects[_key]) {
+                      mergeTarget[objProp] = otherObjects[_key][objProp];
+                    }
+                  };
+                  return mergeTarget;
+                }
+            }
+        },  
+        selector: function(functionList, utilityList) {
             var runOnce = false;
 
             if ( !runOnce ) {
-                sDomQuery.prototype = functionList;
+                //utilityList.extend(functionList, utilityList);
+                sDomQuery.prototype = utilityList;
+                utilityList.extend(sDomQuery.prototype, functionList);
+                utilityList.extend(wrapper, utilityList);
                 runOnce = true;
             }
 
-            function sDomQuery(foundObjects) {
-                this.length = foundObjects ? foundObjects.length : 0;
-
-                if ( foundObjects ) {
-                    for(var i = 0 ; i < foundObjects.length ; i++ ) {
-                        this[i] = foundObjects[i];
-                    } 
-                } 
-            }
-
-            return function(domSelector) {
+            function sDomQuery(domSelector) {
                 var foundObjects = null,
                     regHtml = /^$/,
                     regID = /^$/,
@@ -83,13 +89,26 @@
                     foundObjects = document.querySelectorAll(domSelector);
                 }
 
-                return (function() {
-                    return new sDomQuery(foundObjects);
-                })();
-            };
+                this.length = foundObjects ? foundObjects.length : 0;
+
+                if ( foundObjects ) {
+                    for(var i = 0 ; i < foundObjects.length ; i++ ) {
+                        this[i] = foundObjects[i];
+                    } 
+                } 
+
+                return this;
+            }      
+
+            function wrapper(domSelector) {
+                return new sDomQuery(domSelector);
+            }          
+
+            return wrapper;
         },
-        init: function(moduleList, api) {
+        init: function(moduleList) {
             var functionList = {},
+                utilityList = {},
                 importList = null,
                 $ = null;
 
@@ -102,8 +121,16 @@
                 }
             }
 
-            $ = moduleList['selector'](api, functionList);
+            importList = moduleList['utils']();
+            for ( var _func in importList ) {
+                if ( "undefined" === typeof utilityList[_func] ) {
+                    utilityList[_func] = importList[_func];
+                }
+            }
+
+            $ = moduleList['selector'].call(this, functionList, utilityList);
+
             return $;
         }
-    }, 'init', { /* Default, config, or other imports */ });
+    }, 'init');
 });
