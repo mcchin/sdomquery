cd09193ba0787caf7370432e27046e91dc0cae87
Chin <mcchin@gmail.com>
2015-02-11 10:18:56 +0000

    Testing

--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 Just want a small library to query DOM for micro projects instead of include the entire jQuery.
 
 Inspired by the following links:
+
 1. http://www.sitepoint.com/jquery-vs-raw-javascript-1-dom-forms/
 2. https://github.com/szarouski/lodash.dom-traverse/
 3. https://github.com/ctult/TinyDOM
--- sdomquery.js
+++ sdomquery.js
@@ -1,99 +1,99 @@
 ! function(e) {
-	var obj;
-	"undefined" !== typeof window ? obj = window : "undefined" !== typeof global ? obj = global : "undefined" !== typeof self && (obj = self), obj.$ = e();
+    var obj;
+    "undefined" !== typeof window ? obj = window : "undefined" !== typeof global ? obj = global : "undefined" !== typeof self && (obj = self), obj.$ = e();
 } (function() {
-	return (function loadModule(moduleList, initFunction, api) {		
+    return (function loadModule(moduleList, initFunction, api) {		
       $ = moduleList[initFunction].call(api, moduleList, api);
       return $;
-	})({
-		modules: {
-			css: function() {
-				return {
-					act1: function() {
-						// testing
-						console.log('act1');
-						console.log(this);
-						this['each'](this, function(ele) {
-						    console.log(ele.style);
-						});						
-						return this;
-					},
-					act2: function(domObject) {
-						// testing
-						console.log('act2');
-						return this;
-					}
-				};
-			},
-			utils: function() {
-				return {
-					each: function each(elements, callback) {
-		        var length = elements.length;
-		        if ("undefined" !== length) {
-		          for (var i = 0; i < length; i++) {
-		              callback.call(null, elements[i], i, elements);
-		          }
-		        }
-		        return elements;
-					}
-				}
-			}
-		},
-		selector: function(api, functionList) {
+    })({
+        modules: {
+            css: function() {
+                return {
+                    act1: function() {
+                        // testing
+                        console.log('act1');
+                        console.log(this);
+                        this['each'](this, function(ele) {
+                            console.log(ele.style);
+                        });						
+                        return this;
+                    },
+                    act2: function(domObject) {
+                        // testing
+                        console.log('act2');
+                        return this;
+                    }
+                };
+            },
+            utils: function() {
+                return {
+                    each: function each(elements, callback) {
+                var length = elements.length;
+                if ("undefined" !== length) {
+                  for (var i = 0; i < length; i++) {
+                      callback.call(null, elements[i], i, elements);
+                  }
+                }
+                return elements;
+                    }
+                }
+            }
+        },
+        selector: function(api, functionList) {
 
-			var runOnce = false;
+            var runOnce = false;
 
-			function $(domSelector) {
-				var foundObjects = null,
-					  regHtml = /^$/,
-					  regID = /^$/,
-					  regClass = /^$/;
+            function $(domSelector) {
+                var foundObjects = null,
+                      regHtml = /^$/,
+                      regID = /^$/,
+                      regClass = /^$/;
 
-				// if html append
-				// if instance return instance
-				if ( domSelector ) {
-					foundObjects = document.querySelectorAll(domSelector);
-				}
+                // if html append
+                // if instance return instance
+                if ( domSelector ) {
+                    foundObjects = document.querySelectorAll(domSelector);
+                }
 
-				return (function() {
-					return new sDomQuery(foundObjects);
-				})();
-			}
+                return (function() {
+                    return new sDomQuery(foundObjects);
+                })();
+            }
 
-			if ( !runOnce ) {
-				sDomQuery.prototype = functionList;
-				runOnce = true;
-			}
+            if ( !runOnce ) {
+                sDomQuery.prototype = functionList;
+                runOnce = true;
+            }
 
-			function sDomQuery(foundObjects) {
+            function sDomQuery(foundObjects) {
 
-				this.length = foundObjects ? foundObjects.length : 0;
+                this.length = foundObjects ? foundObjects.length : 0;
 
-				if ( foundObjects ) {
-					for(var i = 0 ; i < foundObjects.length ; i++ ) {
-						this[i] = foundObjects[i];
-					} 
-				} 
-			}
+                if ( foundObjects ) {
+                    for(var i = 0 ; i < foundObjects.length ; i++ ) {
+                        this[i] = foundObjects[i];
+                    } 
+                } 
+            }
 
-			return $;
-		},
-		init: function(moduleList, api) {
-			var functionList = {},
-				  importList = null,
-				  $ = null;
+            return $;
+        },
+        init: function(moduleList, api) {
+            var functionList = {},
+                  importList = null,
+                  $ = null;
 
-			for ( var _key in moduleList['modules']) {
-				importList = moduleList['modules'][_key]();
-				for ( var _func in importList ) {
-					if ( "undefined" === typeof functionList[_func] ) {
-						functionList[_func] = importList[_func];
-					}
-				}
-			}
+            for ( var _key in moduleList['modules']) {
+                importList = moduleList['modules'][_key]();
+                for ( var _func in importList ) {
+                    if ( "undefined" === typeof functionList[_func] ) {
+                        functionList[_func] = importList[_func];
+                    }
+                }
+            }
 
-			$ = moduleList['selector'](api, functionList);
-			return $;
-		}
-	}, 'init', { /* Default, config, or other imports */ });
+            $ = moduleList['selector'](api, functionList);
+            return $;
+        }
+    }, 'init', { /* Default, config, or other imports */ });
 });
